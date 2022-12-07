4835042b3fe8f7e635a01a149fd4d239a769c9c6
Hrijul Bhatnagar <aviusanima@gmail.com>
2022-12-08 05:08:34 +0530

    Solved day 3 puzzle

--- src/day03/Solution.kt
+++ src/day03/Solution.kt
@@ -1,12 +1,45 @@
 package day03
+
 import readInputString
 
+val itemPriorities = generatePriorityMap()
+
 fun part1(input: List<String>): Long {
-    return 0
+    var totalPriority = 0L
+
+    for (line in input) {
+        val firstHalf = line.substring(0, line.length / 2).toSet()
+        val secondHalf = line.substring(line.length / 2).toSet()
+        val commonItem = firstHalf.intersect(secondHalf).first().toString()
+        totalPriority += itemPriorities[commonItem]!!
+    }
+
+    return totalPriority
 }
 
 fun part2(input: List<String>): Long {
-    return 0
+    val groups = input.chunked(3)
+    val commonItemsForGroups: MutableList<String> = mutableListOf() // common items for each 3 elf group
+
+    for (group in groups) {
+        val commonInFirstAndSecond = group[0].toSet().intersect(group[1].toSet())
+        val commonInSecondAndThird = group[1].toSet().intersect(group[2].toSet())
+        val commonItem = commonInFirstAndSecond.intersect(commonInSecondAndThird).first().toString()
+        commonItemsForGroups.add(commonItem)
+    }
+
+    return commonItemsForGroups.sumOf { itemPriorities[it]!! }.toLong()
+}
+
+fun generatePriorityMap(): Map<String, Int> {
+    val priorities: MutableMap<String, Int> = mutableMapOf()
+    var currentPriority = 1
+    for (character in 'a'..'z') {
+        priorities[character.toString()] = currentPriority
+        priorities[character.toString().uppercase()] = currentPriority + 26
+        currentPriority++
+    }
+    return priorities
 }
 
 fun main() {
@@ -17,6 +50,6 @@ fun main() {
     println("Part 2 answer: ${part2(input)}")
 
     // Tests for part 1 and 2
-    check(part1(testInput) == 24000L)
-    check(part2(testInput) == 45000L)
+    check(part1(testInput) == 157L)
+    check(part2(testInput) == 70L)
 }
